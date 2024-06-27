4e22b3d30ab0fa54370af8cd144591dd58cb7eb5
li3b33 <al.barriab@duocuc.cl>
2024-06-27 18:58:21 -0400

    v2

--- MirasolConecta/urls.py
+++ MirasolConecta/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from M_C.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('MirasonConecta/',include(MirasonlConecta.urls)),
+    path('',index),
 ]
