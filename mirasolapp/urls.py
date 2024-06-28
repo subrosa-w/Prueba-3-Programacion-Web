ffb1a34aca3f2e1adef9f4f9b46a162601204b30
Benjamin <ben.sandovalv@duocuc.cl>
2024-06-28 03:14:31 -0400

    version 3.0 (funciona ctmmmmmm q emocion)

--- mirasolapp/urls.py
+++ mirasolapp/urls.py
@@ -4,9 +4,9 @@ from . import views
 #Creamos una lista con URL
 
 urlpatterns = [
+    path('', views.login, name='login'),
     path('index', views.index, name='index'),
     path('noticias', views.noticias, name='noticias'),
     path('servicios', views.servicios, name='servicios'),
-    path('', views.login, name='login'),
     path('formulario', views.formulario, name='formulario'),
 ]
